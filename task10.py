2ca5e9412d0bdd6892a78946ad7da3ea3c109d42
kanat nadyrbekov <kanatnadyrbekov@gmail.com>
2019-12-21 12:59:47 +0600

    Ch1Part2 Task 10

---
+++ task10.py
@@ -0,0 +1,11 @@
+a = input("String:")
+print(a[2])
+print(a[1:len(a)])
+print(a[0:5])
+print(a[0:len(a)-2])
+print(a[1:len(a):2])
+print(a[0:len(a):2])
+print(a[::-1])
+b = a[::-1]
+print(b[1:len(b):2])
+print(len(b))
